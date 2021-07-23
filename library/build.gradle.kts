3d864433708db5b88fa235b664a086718306a2c4
warting <stefan@warting.se>
2021-07-23 10:26:38 +0200

    compose tooling rc2

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -44,9 +44,9 @@ android {
 }
 
 dependencies {
-    implementation("androidx.compose.ui:ui-tooling:1.0.0-rc01")
+    implementation("androidx.compose.ui:ui-tooling:1.0.0-rc02")
     implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.3.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("com.google.android.material:material:1.4.0")
 }
 
