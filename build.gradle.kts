a913874abaf39617e6a5e9b52343b5341fb877b8
warting <stefan@warting.se>
2022-10-30 21:49:34 +0100

    ktx build.gradle

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+buildscript {
+    repositories {
+        maven {
+            // until broken gradle plugin portal is fixed
+            url = uri("https://premex.jfrog.io/artifactory/local-gradle-plugins/")
+        }
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
